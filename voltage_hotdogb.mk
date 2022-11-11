576f8f75d2345bb0b5f80412f9951a3fe003df61
ghost-707 <ghost.707.gaming@gmail.com>
2022-11-11 20:40:26 +0530

    initial voltage bringup

--- voltage_hotdogb.mk
+++ voltage_hotdogb.mk
@@ -11,18 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hotdogb device
 $(call inherit-product, device/oneplus/hotdogb/device.mk)
 
-# Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
-# Setup Derp options
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-
-# Official-ify
-DERP_BUILDTYPE := Official
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_hotdogb
+PRODUCT_NAME := voltage_hotdogb
 PRODUCT_DEVICE := hotdogb
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := HD1905
@@ -33,6 +26,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Boot animation
+TARGET_BOOT_ANIMATION_RES := 1920
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
@@ -44,3 +38,21 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7T_EEA-user 10 QKQ1.190716.003 1909250940 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+
+# Gapps
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+
+# Voltage stuff
+GrapheneApps := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
+VOLTAGE_MAINTAINER := Ghost
+TARGET_EXCLUDES_AUDIOFX := true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.voltage.maintainer=Ghost
+
+# Oneplus Camera and Gallery
+$(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
+TARGET_SHIP_OOSCAM := true
+TARGET_SHIP_OOSGALLERY := true
