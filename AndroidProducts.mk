036765a62f93a861ee6a68da8fd154d854ede4a0
ghost-707 <ghost.707.gaming@gmail.com>
2022-11-20 16:47:52 +0530

    initial adapt aicp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_hotdogb.mk
+    $(LOCAL_DIR)/aicp_hotdogb.mk
 
 COMMON_LUNCH_CHOICES := \
-				    derp_hotdogb-user \
-				    derp_hotdogb-userdebug \
-				    derp_hotdogb-eng
+				    aicp_hotdogb-userdebug \
+				    aicp_hotdogb-eng
--- aicp_hotdogb.mk
+++ aicp_hotdogb.mk
@@ -11,18 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hotdogb device
 $(call inherit-product, device/oneplus/hotdogb/device.mk)
 
-# Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
-# Setup Derp options
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-
-# Official-ify
-DERP_BUILDTYPE := Official
+# Inherit some common aicp stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_hotdogb
+PRODUCT_NAME := aicp_hotdogb
 PRODUCT_DEVICE := hotdogb
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := HD1905
@@ -33,6 +26,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
@@ -44,3 +38,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7T_EEA-user 10 QKQ1.190716.003 1909250940 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# aicp stuff
+WITH_GMS := true
+BUILD_TWRP := true
+AICP_BUILDTYPE := UNOFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.aicp.maintainer=Ghost
