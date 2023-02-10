8965d75325df4214c9db297313f7b82426a7bb1a
ghost-707 <ghost.707.gaming@gmail.com>
2023-02-11 02:18:25 +0530

    initial bringup banana

--- banana_hotdogb.mk
+++ banana_hotdogb.mk
@@ -11,18 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hotdogb device
 $(call inherit-product, device/oneplus/hotdogb/device.mk)
 
-# Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
-# Setup Derp options
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-
-# Official-ify
-DERP_BUILDTYPE := Official
+# Inherit some common Banana stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_hotdogb
+PRODUCT_NAME := banana_hotdogb
 PRODUCT_DEVICE := hotdogb
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := HD1905
@@ -35,7 +28,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
-USE_LEGACY_BOOTANIMATION := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Build info
 BUILD_FINGERPRINT := "OnePlus/OnePlus7T_CH/OnePlus7T:11/RKQ1.201022.002/2206241702:user/release-keys"
@@ -45,3 +38,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7T_EEA-user 10 QKQ1.190716.003 1909250940 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# Banana Stuff
+BANANA_MAINTAINER := Ghost
+BANANA_BUILD_TYPE := UNOFFICIAL
+WITH_GAPPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
